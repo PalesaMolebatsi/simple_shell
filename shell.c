8b75d301c3dbc780af7777c9045c87cea8ea85f8
Bongekile06 <mlindwab@gmail.com>
2023-08-14 14:07:59 -0700

    updated

--- shell.c
+++ shell.c
@@ -1,73 +1,50 @@
 #include "holberton.h"
-
 /**
- * main - Entry point for the shell program.
- * Return: 0 on success.
+ * main - This is a simple shell created by
+ * Bongekile Mlindwa and Palesa Molebatsi
+ * for ALX SE PROGRAM
+ * Return: 0 if success
  */
 int main(void)
 {
-	ssize_t rd_bytes = 0; /* Bytes read from getline */
-	size_t buffer_size = 0; /* Buffer size */
-	char *input = NULL, *args[20]; /* User input arguments */
-	int cmd_count = 1, path_stat = 0, exist_stat = 0, exit_status = 0, builtin_stat = 0;
+	ssize_t bytes_rd = 0; /**Bytes leídos de un getline*/
+	size_t bf_size = 0; /**Bytes read from a getline*/
+	char *entry = NULL, *arguments[20]; /**String of arguments entered by the user*/
+	int counter = 1, vf_stat = 0, exist_stat = 0, exit_stat = 0, blt_stat = 0;
 
-	_print_prompt("$ ", 2); /* Display prompt for the shell */
-	rd_bytes = getline(&input, &buffer_size, stdin); /* Read user input */
-
-	while (rd_bytes != -1)
+	_printp("$ ", 2);/**prompt mini-shell*/
+	bytes_rd = getline(&entry, &bf_size, stdin); /**sizeof entry, o -1 (EOF))*/
+	while (bytes_rd != -1)
 	{
-		if (*input != '\n')
+		if (*entry != '\n')
 		{
-			fill_arguments(input, args);
-
-			if (args[0] != NULL)
+			fill_args(entry, arguments);
+			if (arguments[0] != NULL)
 			{
-				path_stat = check_path(args[0]); /* Check if the path exists */
-
-				if (path_stat != 0) /* File not found */
+				exist_stat = exist(arguments[0]);/**Exist evaluates whether the entered path exists*/
+				if (exist_stat != 0)/**File not found*/
 				{
-					exist_stat = verify_path_existence(args);
-
-					if (exist_stat == 0)
-					{
-						exit_status = execute_command(args);
-						free(input);
-						free(*args);
-					}
+					vf_stat = verify_path(arguments);
+					if (vf_stat == 0)
+						exit_stat = exec(arguments), free(entry), free(*arguments);
 					else
 					{
-						builtin_stat = check_builtin(args, exit_status);
-
-						if (builtin_stat != 0)
-						{
-							exit_status = print_not_found(args, cmd_count);
-							free(input);
-						}
+					blt_stat = verify_blt(arguments, exit_stat);
+					if (blt_stat != 0)
+						exit_stat = print_not_found(arguments, counter), free(entry);
 					}
 				}
-				else /* File found */
-				{
-					exit_status = execute_command(args);
-					free(input);
-				}
+				else /**File not found*/
+					exit_stat = exec(arguments), free(entry);
 			}
 			else
-			{
-				free(input);
-			}
+				free(entry);
 		}
-		else if (*input == '\n')
-		{
-			free(input);
-		}
-
-		input = NULL;
-		cmd_count++;
-		_print_prompt("$ ", 2);
-		rd_bytes = getline(&input, &buffer_size, stdin);
+		else if (*entry == '\n')
+			free(entry);
+		entry = NULL, counter++;
+		_printp("$ ", 2), bytes_rd = getline(&entry, &bf_size, stdin);
 	}
-
-	clean_up(input);
-	return (exit_status);
+	last_free(entry);
+	return (exit_stat);
 }
-
