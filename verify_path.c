0e8f2c1ee06ef50bfe2b76c74fe07975648ab724
Bongekile06 <mlindwab@gmail.com>
2023-08-14 14:12:38 -0700

    updated

--- verify_path.c
+++ verify_path.c
@@ -1,57 +1,45 @@
 #include "holberton.h"
 /**
- * check_path - Verify if the first command can be executed.
- * @args: Array of entries by the user.
- * Return: 0 if success, or -1 if the executable file doesn't exist.
+ * verify_path - Verify if the first command can be executed
+ * @arguments: Array of entries by the user
+ * Return: 0 if success, or -1 if the exe file doesn't exist
  */
-int check_path(char **args)
+int verify_path(char **arguments)
 {
-	char *global_path = NULL;
+	char *global_path = NULL; /**"/usr/bin:/bin:/usr/games:/usr/local/games"*/
 	char *global_dup = NULL;
-	char *dir_path = NULL;
-	char *command_path = NULL;
-	char *test_paths[121];
+	char *dir_path = NULL; /**"   /usr/bin    "*/
+	char *command_path = NULL; /**"   /usr/bin/touch      "*/
+	char *test_cph[121];
 	int exist_stat = -1, i = 0;
 
-
-	global_path = get_env_variable("PATH");
+	global_path = _getenv("PATH");
 	if (global_path == NULL)
 		return (-1);
-
-
-	global_dup = duplicate_string(global_path);
-	if (global_dup == NULL)
-	{
-		free(global_path);
-		return (-1);
-	}
-
-
+	global_dup = (_strdup(global_path));
 	dir_path = strtok(global_dup, ":");
+	if (dir_path == NULL)
+		return (-1);
+	free(global_path);
 	while (exist_stat == -1 && dir_path != NULL)
 	{
-		command_path = append_command(dir_path, args[0]);
-		test_paths[i] = command_path;
-		exist_stat = check_existence(test_paths[i]);
+		command_path = append_command(dir_path, arguments[0]);
+		test_cph[i] = command_path;
+		exist_stat = exist(test_cph[i]);
 		dir_path = strtok(NULL, ":");
 		i++;
 	}
 	i--;
-
-
-	free(global_path);
 	free(global_dup);
-	free_grid(test_paths, i + 1);
-
-
+	free_grid(test_cph, i);
 	if (exist_stat == 0)
 	{
-		args[0] = test_paths[i];
+		arguments[0] = test_cph[i];
 		return (0);
 	}
 	else
 	{
-		free(test_paths[i]);
+		free(test_cph[i]);
 		return (-1);
 	}
 }
